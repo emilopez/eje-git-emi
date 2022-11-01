0e0306a14b129043db1d92b71ae4eeada31d2a36
Emiliano Lopez <emiliano.lopez@gmail.com>
2022-11-01 20:31:47 +0100

    agrego suma de funciones

--- primer-programa-vscode.py
+++ primer-programa-vscode.py
@@ -10,5 +10,7 @@ f2 = np.cos(2*np.pi*t)
 fig = go.Figure()
 fig.add_trace(go.Scattergl(x=t, y=f1, mode="lines", name="sin()"))
 fig.add_trace(go.Scattergl(x=t, y=f2, mode="lines", name="cos()"))
+fig.add_trace(go.Scattergl(x=t, y=f1+f2, mode="lines", name="suma"))
+
 
 fig.show()
